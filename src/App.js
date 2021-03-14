ee9d64c0f7bc73aac494bd2d18bedeb125e2654a
Nurfaidah636 <70933551+Nurfaidah636@users.noreply.github.com>
2021-03-15 04:39:16 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicExample from './jobsheet/BasicExample';
+import ParamsExample from './jobsheet/ParamsExample';
+import NestingExample from './jobsheet/NestingExample';
+import AuthExample from './jobsheet/AuthExample';
+import OnlineShop from './jobsheet/OnlineShop';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <BasicExample /> */}
+      {/* <ParamsExample /> */}
+      {/* <NestingExample /> */}
+      {/* <AuthExample /> */}
+      <OnlineShop />
+    </div>
+  );
+}
+
+export default App;
